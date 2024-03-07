a9c63b0eae5c2b7569ae259e39d35aec88040c45
Pachaymhere <03230218.gcbs@rub.edu.bt>
2024-03-07 22:03:47 +0600

    vote

---
+++ CheckVote.py
@@ -0,0 +1,23 @@
+# ! check if user can vote
+
+# ?Get user age from input
+# ?convert userinput(str) to int() to number
+# ?check if user can vote
+    # ?if else statement
+    # ?if above 18, print "You can vote"
+    # ?if below 18, print "You canot vote"
+    # get user input
+userInput = input('plesase type your age:')
+# convert user input to number
+userAge = int(userInput)
+#Check if user can vote
+if userAge >= 18:
+    print('You can vote')
+else:
+    print('You cannot vote')
+
+# 3. Check if user can vote
+if userAge > 18:
+    print('You can vote')
+elif userAge < 18:
+    print('You cannot vote')
